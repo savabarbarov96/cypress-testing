1827cf193666d06836988b24e5824227cc0c56e3
Sava Barbarov <savabarbarov96@gmail.com>
2022-09-23 15:38:46 +0300

    Adding cypress-file-upload option

--- cypress/e2e/pmodemo.cy.js
+++ cypress/e2e/pmodemo.cy.js
@@ -3,16 +3,12 @@ import { LoginPage } from "./pages/loginPage";
 // best practice to include Page Object Model
 const logingPage = new LoginPage();
 
-
-//best practices before each 
+//best practices before each
 beforeEach(() => {
   cy.visit(
     "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
   );
-})
-
-
-
+});
 
 describe("All Login Tests", () => {
   it("Login Test 1", () => {
@@ -23,7 +19,7 @@ describe("All Login Tests", () => {
   });
 
   it("Login Test 2", () => {
-    // cy.get("[name='username']").click().type("Admin") // 
+    // cy.get("[name='username']").click().type("Admin") //
     logingPage.enterUsername("Admin");
     logingPage.enterPassword("admin123");
     logingPage.clickLogin();
@@ -31,16 +27,28 @@ describe("All Login Tests", () => {
   });
 
   it("Search employee", () => {
-    // cy.get("[name='username']").click().type("Admin") // 
+    // cy.get("[name='username']").click().type("Admin") //
     logingPage.enterUsername("Admin");
     logingPage.enterPassword("admin123");
     logingPage.clickLogin();
     logingPage.verifyLogin();
-    logingPage.findEmployee("0038")
-    cy.get('.oxd-form-actions > .oxd-button--secondary').click({force: true})
-    cy.wait(500)
-    cy.get('.oxd-table-card > .oxd-table-row').click({ multiple: true })
-    cy.get('.orangehrm-edit-employee-name > .oxd-text').should('have.text', 'Aaliyah Haq')
+    // logingPage.findEmployee("0315")
+    // cy.get('.oxd-form-actions > .oxd-button--secondary').click({force: true})
+    // cy.wait(500)
+    // cy.get('.oxd-table-card > .oxd-table-row').click({ multiple: true })
+    // cy.get('.orangehrm-edit-employee-name > .oxd-text').should('have.text', 'Sava B')
+  });
+
+  it("Upload Photo", () => {
+    // cy.get("[name='username']").click().type("Admin") //
+
+    const filepath = "test.png";
+    logingPage.enterUsername("Admin");
+    logingPage.enterPassword("admin123");
+    logingPage.clickLogin();
+    logingPage.verifyLogin();
+    cy.get(":nth-child(6) > .oxd-main-menu-item").click();
+    cy.get(".employee-image").click();
+    cy.get("input[type=file]").attachFile(filepath);
   });
 });
-
