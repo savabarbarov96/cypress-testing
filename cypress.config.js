5541813183f2aa2d1baa552403fed8628905fe69
Sava Barbarov <savabarbarov96@gmail.com>
2022-09-24 11:31:35 +0300

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       watchForFileChanges: false
       // implement node event listeners here
     },
